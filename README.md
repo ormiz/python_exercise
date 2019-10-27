b104a9e3bc0f797dd7e49a28603c9bf3721a389f
Orm <or.mizrahi@gingersoftware.com>
2019-10-27 18:27:14 +0200

    Final code

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # python_exercise
+
+To pull docker image:
+docker pull ormi2/python-exercise
+
+I used Flask as my server framework and took part of the code from https://github.com/shantnu/FaceDetect for detection.
+The main file is face_detect_server.py.
+
+The program can get port number from outside, for example:
+python face_detect_server.py 5000.
+
+Since the program can receive the port argument, You can specify the port inside the Dockerfile in the CMD line.
+To build the docker image:
+docker build -t python-exercise:latest .
+To run a container with port 5000 (should match the port inside the Dockerfile):
+docker run -p 5000:5000 python-exercise
+
+The main endpoint of the server is:
+http://{server}:{port}/detectFaces/{encodedURL}
+
+for example:
+http://localhost:5000/detectFaces/https%3A%2F%2Fclemenssiebler.com%2Fwp-content%2Fuploads%2F2018%2F09%2Fface_api_test.jpg
+
+This should return a string "Faces: 2"
--- face_detect_server.py
+++ face_detect_server.py
@@ -1,8 +1,55 @@
 from flask import Flask, escape, request
-from face_detect import detectFaces
+import numpy as np
+import urllib
+import urllib2
+import cv2
+import sys
 
 app = Flask(__name__)
 
+# Get user supplied values
+cascPath = "haarcascade_frontalface_default.xml"
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+def url_to_image(url):
+	# download the image, convert it to a NumPy array, and then read
+	# it into OpenCV format
+	req = urllib2.Request(url)
+	try: resp = urllib2.urlopen(req)
+	except urllib2.HTTPError as e:
+		raise Exception('The server couldn\'t fulfill the request. Error code: {0}'.format(e.code))
+	except urllib2.URLError as e:
+		raise Exception('Failed to reach server. Reason: {0}'.format(e.reason))
+		
+	image = np.asarray(bytearray(resp.read()), dtype="uint8")
+	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+	if image is None:
+		raise Exception('Invalid image format')
+	# return the image
+	return image
+
+def detectd_faces(imageUrl):
+
+	# Download the image
+	image = url_to_image(imageUrl)
+	
+	# Make it gray
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	
+	# Detect faces in the image
+	faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30, 30))
+	# Return the length of the array
+	return len(faces)
+	
+
 @app.route('/detectFaces/<path:url>')
-def detectFacesEndpoint(url):
-    return detectFaces(url)
+def detect_faces_endpoint(url):
+	try:
+		detected_faces = detectd_faces(url)
+		return "Faces: {}".format(detected_faces)
+	except Exception as e:
+		return str(e)
+
+port = int(sys.argv[1])		
+app.run(host='0.0.0.0', port=port)
